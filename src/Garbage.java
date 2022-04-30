c3780573af3d1a0a2bdf97ad45e2aa7e683f49ee
IgorVoskresensky <isysenko@yandex.ru>
2022-04-30 13:09:34 +0300

    Initial commit

---
+++ src/Garbage.java
@@ -0,0 +1,14 @@
+public class Garbage {
+
+//        Map<String, Integer> map = new HashMap<>(Map.of(
+//                "Google", 550,
+//                "DSV", 1200,
+//                "Gl", 100000
+//        ));
+//        map.merge("Google", 50, Integer::sum);
+//        map.merge("Health Inc", 100, Integer::sum);
+//        map.compute("Gl", (comp, emp) -> (int) (emp * 0.8));
+//        map.computeIfPresent("Health Inc", (comp, emp) -> emp + 200);
+//        map.forEach((k, v) -> System.out.println(k + "->" + v));
+
+}
---
+++ src/Test.java
@@ -0,0 +1,59 @@
+import java.lang.reflect.Array;
+import java.util.*;
+import java.util.concurrent.*;
+
+public class Test {
+
+
+    public static void main(String[] args) throws InterruptedException {
+
+        int[] a = new int[] {1, 4, 232, 434, 56, 1, 56, 75, 87, -1, 0};
+        mergeSorting(a);
+
+        System.out.println(Arrays.toString(a));
+
+
+    }
+
+    public  static  void mergeSorting(int[] a){
+
+        if(a.length > 1){
+            int middle = a.length / 2;
+            int [] left = new int[middle];
+            int [] right = new int[a.length - middle];
+
+            for (int i = 0; i < middle; i++) {
+                left[i] = a[i];
+            }
+            for (int i = middle; i < a.length; i++){
+                right[i - middle] = a[i];
+            }
+                mergeSorting(left);
+                mergeSorting(right);
+                merge(a, left, right);
+            }
+        }
+
+    public static void merge(int[] a, int[] left, int[] right) {
+        int i = 0;
+        int j = 0;
+        int index = 0;
+        while (i < left.length && j < right.length){
+            if(left[i] < right[j]){
+                a[index] = left[i];
+                i++;
+            } else {
+                a[index] = right[j];
+                j++;
+            }
+            index++;
+        }
+        for (int leftRest = i; leftRest < left.length; leftRest++){
+            a[index++] = left[leftRest];
+        }
+        for (int rightRest = j; rightRest < right.length; rightRest++){
+            a[index++] = right[rightRest];
+        }
+
+    }
+}
